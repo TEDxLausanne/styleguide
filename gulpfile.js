d44187d2549553743d4ba987740859b49195b74d
Marc Friederich <marc@antistatique.net>
2014-11-08 10:24:30 +0100

    Issue with minimfying CSS ...

--- gulpfile.js
+++ gulpfile.js
@@ -93,7 +93,7 @@ gulp.task('styles', function() {
       .on('error', notify.onError("Error: <%= error.message %>"))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(gulp.dest('build/css'))
-    .pipe(minifycss())
+    // .pipe(minifycss())
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('build/css'));
 });
